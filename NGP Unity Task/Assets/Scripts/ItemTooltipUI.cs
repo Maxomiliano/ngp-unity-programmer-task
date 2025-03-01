1ee78ae1c5670880d197d7b0420f1a9c40242e7c
Maxomiliano <max.nebula1@gmail.com>
2025-03-01 12:52:29 -0300

    TooltipUI

--- NGP Unity Task/Assets/Scripts/ItemTooltipUI.cs
+++ NGP Unity Task/Assets/Scripts/ItemTooltipUI.cs
@@ -8,10 +8,38 @@ public class ItemTooltipUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _itemNameText;
     [SerializeField] private TextMeshProUGUI _itemDescriptionText;
-    [SerializeField] private TextMeshProUGUI _itemStatsText;
+    [SerializeField] private TextMeshProUGUI _itemDamageText;
+    [SerializeField] private TextMeshProUGUI _itemResistanceText;
     [SerializeField] private Button _equipButton;
     [SerializeField] private Button _dropButton;
 
     private ItemDataSO _currentItem;
     private Inventory _inventory;
+
+    private void Start()
+    {
+        //Equip and drop button functionality
+    }
+
+    private void Awake()
+    {
+        _inventory = FindObjectOfType<Inventory>();
+        gameObject.SetActive(false);
+    }
+
+    public void ShowTooltip(ItemDataSO item, Vector2 position)
+    {
+        _currentItem = item;
+        _itemNameText.text = item.itemName;
+        _itemDescriptionText.text = item.itemDescription;
+        _itemDamageText.text = item.damage.ToString();
+        _itemResistanceText.text = item.resistance.ToString();
+        transform.position = position;
+        gameObject.SetActive(true);
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
 }
