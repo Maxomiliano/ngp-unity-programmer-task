777937d3053ea3371cf5b1c7e9572ea924884f6f
Maxomiliano <max.nebula1@gmail.com>
2025-03-02 13:41:02 -0300

    Character animations, items sprites and inventory background

--- NGP Unity Task/Assets/Scripts/PlayerController.cs
+++ NGP Unity Task/Assets/Scripts/PlayerController.cs
@@ -13,25 +13,38 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _interactRadius = 2f;
     
     private Rigidbody2D _rb;
+    private Animator _animator;
     private bool _isGrounded;
 
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>(); 
     }
 
     private void Update()
     {
+        Run();
+        Jump();
+        PickItem();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
         float move = Input.GetAxis("Horizontal");
         _rb.velocity = new Vector2(move * _speed, _rb.velocity.y);
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rb.velocity.x) > Mathf.Epsilon;
+        _animator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+    private void Jump()
+    {
         _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundLayer);
-
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
         }
-        PickItem();
     }
 
     private void PickItem()
@@ -45,4 +58,14 @@ public class PlayerController : MonoBehaviour
             }
         }
     }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(_rb.velocity.x), 1f);
+        }
+    }
+
 }
